0df43583ab4a4ae730cdf73b6507b119332b1629
Tejassingh29 <tejas.singh@kalvium.community>
2026-03-30 11:56:40 +0530

    implemented multi-screen navigation using Navigator and routes

--- RuralEduTrack/lib/main.dart
+++ RuralEduTrack/lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-
-import 'screens/responsive_home.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'screens/login_screen.dart';
+import 'screens/home_screen.dart';
+import 'screens/second_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,147 +10,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: WelcomeScreen(),
       debugShowCheckedModeBanner: false,
-    );
-  }
-}
 
-class WelcomeScreen extends StatefulWidget {
-  @override
-  _WelcomeScreenState createState() => _WelcomeScreenState();
-}
+      initialRoute: '/',
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
-  bool isClicked = false;
-
-  void toggleText() {
-    setState(() {
-      isClicked = !isClicked;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Welcome App'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              isClicked ? 'Welcome Back!' : 'Hello Flutter!',
-              style: TextStyle(fontSize: 24),
-            ),
-            SizedBox(height: 20),
-            Icon(
-              Icons.flutter_dash,
-              size: 100,
-              color: Colors.blue,
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: toggleText,
-              child: Text('Click Me'),
-            )
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: ResponsiveHome(),
-      debugShowCheckedModeBanner: false,
-    );
-  }
-}
-
-
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: LoginScreen(),
-      debugShowCheckedModeBanner: false,
-    );
-  }
-}
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: WidgetTreeDemo(),
-      debugShowCheckedModeBanner: false,
-    );
-  }
-}
-
-class WidgetTreeDemo extends StatefulWidget {
-  @override
-  _WidgetTreeDemoState createState() => _WidgetTreeDemoState();
-}
-
-class _WidgetTreeDemoState extends State<WidgetTreeDemo> {
-  bool isClicked = false;
-
-  void toggleState() {
-    setState(() {
-      isClicked = !isClicked;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Widget Tree Demo'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              isClicked ? 'State Changed!' : 'Initial State',
-              style: TextStyle(fontSize: 24),
-            ),
-            SizedBox(height: 20),
-            Icon(
-              Icons.flutter_dash,
-              size: 100,
-              color: isClicked ? Colors.green : Colors.blue,
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: toggleState,
-              child: Text('Toggle State'),
-            )
-          ],
-        ),
-      ),
+      routes: {
+        '/': (context) => HomeScreen(),
+        '/second': (context) => SecondScreen(),
+      },
     );
   }
 }
